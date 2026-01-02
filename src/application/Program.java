f4954621fadaf77b1350267e533e05c2b03e1000
caio-varandas <caiovarandas@hotmail.com>
2026-01-02 16:10:42 -0300

    Implementação update

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,5 +31,12 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TESTE 5: Seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        seller.setEmail("martha@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
