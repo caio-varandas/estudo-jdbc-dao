2005d4ebd364c3e47d0c4e241053cc3bdfa3d444
caio-varandas <caiovarandas@hotmail.com>
2026-01-02 14:11:48 -0300

    Implementação inserção

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,5 +26,10 @@ public class Program {
         System.out.println("\n=== TESTE 3: Seller findAll ===");
         list = sellerDao.findAll();
         list.forEach(System.out::println);
+
+        System.out.println("\n=== TESTE 4: Seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
